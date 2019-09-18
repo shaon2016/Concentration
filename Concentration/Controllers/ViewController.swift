a34101f08937bea960ca5c6c881a8bbc0c74f085
Shaon <shaondeveloper@gmail.com>
2019-09-18 11:59:57 +0600

    class 5 added

--- Concentration/Controllers/ViewController.swift
+++ Concentration/Controllers/ViewController.swift
@@ -11,14 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: Variable and instances
-    lazy var game = Concentration(numberOfParisOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
     
     @IBOutlet var cardButtons: [UIButton]!
-    @IBOutlet weak var flipLabel: UILabel!
+    @IBOutlet weak var flipLabel: UILabel! {
+        didSet {
+            flipCount = 0
+        }
+    }
     var flipCount = 0 {
         didSet {
-            flipLabel.text  = "Flips: \(flipCount)"
+            let attributes: [NSAttributedString.Key: Any] = [
+                .strokeWidth: 5.0,
+                .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            ]
+            let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+            flipLabel.attributedText = attributedString
         }
     }
     
@@ -54,18 +66,33 @@ class ViewController: UIViewController {
         }
     }
 
-    var emojiChoices = ["👀", "🍏", "🍇", "🌝", "🐱", "🐛",  "🐸",  "🙈",  "🐟",
-     "🐦",  "🐅",  "🐗",  "🦕",  "🐣",  "🐳",  "🦗"]
+   private var emojiChoices = "🦇😱🙀😈🎃👻🍭🍬🍎"
     
-    var emoji = [Int : String]()
+    private var emoji = [Card: String]()
     
-    func emoji(for card : Card) -> String {
-        if emoji[card.identifier] == nil , emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+    private func emoji(for card: Card) -> String {
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            let stringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4Random)
+            emoji[card] = String(emojiChoices.remove(at: stringIndex))
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
-
 }
+
+extension Int {
+    var arc4Random: Int {
+        switch self {
+        case 1...Int.max:
+            return Int(arc4random_uniform(UInt32(self)))
+        case -Int.max..<0:
+            return Int(arc4random_uniform(UInt32(self)))
+        default:
+            return 0
+        }
+        
+    }
+}
+
+
+
 
--- Concentration/Model/Concentration.swift
+++ Concentration/Model/Concentration.swift
@@ -11,41 +11,74 @@ import Foundation
 class Concentration {
     var cards  = [Card]()
     // Hold One faced up card index
-    var indexOfOneAndOnlyFacedUpCard : Int?
-    
-    func chooseCard(at index : Int)  {
-        if let matchedCardIndex = indexOfOneAndOnlyFacedUpCard, index != matchedCardIndex {
-            // indexOfOneAndOnlyFacedUpCard holds a card index
-            // current choose card is another card
-            if cards[index].identifier == cards[matchedCardIndex].identifier {
-                cards[index].isMatched = true
-                cards[matchedCardIndex].isMatched = true
-            }
-            // Current chosen card faced up. So it must be true
-            cards[index].isFaceUp = true
-            // Two card was shown. Only facedUpCard Variable must be set to nil.
-            indexOfOneAndOnlyFacedUpCard = nil
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
-        else {
-            // Either no cards or 2 cards are face up
-            
-            // First all card are reset
-            for flipDownIndex in cards.indices {
-                cards[flipDownIndex].isFaceUp = false
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
             }
-            // A card must be faced up
-            cards[index].isFaceUp = true
-            // Hold the faced up card index for later matching
-            indexOfOneAndOnlyFacedUpCard = index
         }
-    
+        
     }
     
-    init(numberOfParisOfCards : Int) {
-        for _ in 1...numberOfParisOfCards {
+//    func chooseCard(at index : Int)  {
+//        if let matchedCardIndex = indexOfOneAndOnlyFacedUpCard, index != matchedCardIndex {
+//            // indexOfOneAndOnlyFacedUpCard holds a card index
+//            // current choose card is another card
+//            if cards[index].identifier == cards[matchedCardIndex].identifier {
+//                cards[index].isMatched = true
+//                cards[matchedCardIndex].isMatched = true
+//            }
+//            // Current chosen card faced up. So it must be true
+//            cards[index].isFaceUp = true
+//            // Two card was shown. Only facedUpCard Variable must be set to nil.
+//            indexOfOneAndOnlyFacedUpCard = nil
+//        }
+//        else {
+//            // Either no cards or 2 cards are face up
+//
+//            // First all card are reset
+//            for flipDownIndex in cards.indices {
+//                cards[flipDownIndex].isFaceUp = false
+//            }
+//            // A card must be faced up
+//            cards[index].isFaceUp = true
+//            // Hold the faced up card index for later matching
+//            indexOfOneAndOnlyFacedUpCard = index
+//        }
+//
+//    }
+    
+    func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)) : Choosen index out of range")
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex] == cards[index] {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            } else {
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)) : You must have at least one pair of cards")
+        for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
         }
-        // TODO: Shuffle the cards
+        //    TODO: Shuffle the cards
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
